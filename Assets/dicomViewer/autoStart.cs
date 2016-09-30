870f49994e62774190dd35d662736c681ec061b3
Dez <lfagames@gmail.com>
2016-09-30 15:51:09 -0400

    added auto start game object and build

---
+++ Assets/dicomViewer/autoStart.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class autoStart : MonoBehaviour {
+
+	public GameObject[] hideOnStart;
+	public dicomManager dicom;
+
+	public bool autoplay = true;
+
+	// Use this for initialization
+	void Start () {
+		dicom.load ();
+
+		if (autoplay)
+			dicom.play ();
+
+		foreach (GameObject g in hideOnStart)
+			g.SetActive (false);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
